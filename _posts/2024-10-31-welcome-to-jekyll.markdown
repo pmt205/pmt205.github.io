9b79b76b286eb3565c04e2e110ec3fc8fce64385
Triet Phan <minhtriet1234@gmail.com>
2024-11-01 17:58:42 +0700

    Adjust Sidebar Desktop

--- _posts/2024-10-31-welcome-to-jekyll.markdown
+++ _posts/2024-10-31-welcome-to-jekyll.markdown
@@ -7,7 +7,7 @@ categories: jekyll update
 {:toc}
 
 # Header 1
-## Header 1.11111111111111111
+## Header 1.135135135135135135135135135135135135
 You’ll find this post in your `_posts` directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run `jekyll serve`, which launches a web server and auto-regenerates your site when a file is updated.
 
 Jekyll requires blog post files to be named according to the following format:
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,35 +1,34 @@
 function scrollToTop() {   
     document.getElementById("top").scrollIntoView();
 }
+if (screen.width <= "800px"){
+    var sidebar = document.getElementById("sidebar");
+    var closetoc = document.getElementById("close-toc");
+    var toTopMobile = document.getElementById("toTop-mobile");
+    var opentoc = document.getElementById("open-toc");
+    const closegroup = [sidebar, closetoc, toTopMobile]
 
-var sidebar = document.getElementById("sidebar");
-var closetoc = document.getElementById("close-toc");
-var toTopMobile = document.getElementById("toTop-mobile");
-var opentoc = document.getElementById("open-toc");
-const closegroup = [sidebar, closetoc, toTopMobile]
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-closegroup.forEach(element => {
-    element.style.opacity = 0;
-    element.style.visibility = "hidden";
-});
-
-opentoc.onclick = async function(){
     closegroup.forEach(element => {
-        element.animate({opacity:[0,1],visibility:["hidden","visible"]},{duration:400,fill:'forwards'});
-        element.style.visibility = "visible";
+        element.style.opacity = 0;
+        element.style.visibility = "hidden";
     });
-    opentoc.animate({opacity:[1,0],visibility:["visible","hidden"]},{duration:400,fill:'forwards'});
-    opentoc.style.visibility = "hidden";
+
+    opentoc.onclick = async function(){
+        closegroup.forEach(element => {
+            element.animate({opacity:[0,1],visibility:["hidden","visible"]},{duration:400,fill:'forwards'});
+        });
+        opentoc.animate({opacity:[1,0],visibility:["visible","hidden"]},{duration:400,fill:'forwards'});
+    }
+
+    closetoc.onclick = async function(){
+        closegroup.forEach(element => {
+            element.animate({opacity:[1,0],visibility:["visible","hidden"]},{duration:400,fill:'forwards'});
+        });
+        opentoc.animate({opacity:[0,1],visibility:["hidden","visible"]},{duration:400,fill:'forwards'});
+    }
 }
 
-closetoc.onclick = async function(){
-    closegroup.forEach(element => {
-        element.animate({opacity:[1,0],visibility:["visible","hidden"]},{duration:400,fill:'forwards'});
-    });
-    opentoc.animate({opacity:[0,1],visibility:["hidden","visible"]},{duration:400,fill:'forwards'});
-    opentoc.style.visibility = "visible";
-}
