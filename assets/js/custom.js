2ca70d729bb13e6bdac8a5c688e77e3b7b439898
Triet Phan <minhtriet1234@gmail.com>
2024-11-01 15:41:27 +0700

    Add Mobile CSS

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,37 @@
 function scrollToTop() {   
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    document.getElementById("top").scrollIntoView();
+}
+
+var sidebar = document.getElementById("sidebar");
+var closetoc = document.getElementById("close-toc");
+var toTopMobile = document.getElementById("toTop-mobile");
+var opentoc = document.getElementById("open-toc");
+
+sidebar.style.opacity = 0;
+sidebar.style.visibility = "hidden";
+closetoc.style.opacity = 0;
+closetoc.style.visibility = "hidden";
+toTopMobile.style.opacity = 0;
+toTopMobile.style.visibility = "hidden";
+
+opentoc.onclick = function(){
+    sidebar.classList.add('fade-in');
+    closetoc.classList.add('fade-in');
+    toTopMobile.classList.add('fade-in');
+    opentoc.classList.add('fade-out');
+    sidebar.classList.remove('fade-out');
+    closetoc.classList.remove('fade-out');
+    toTopMobile.classList.remove('fade-out');
+    opentoc.classList.remove('fade-in');
+}
+
+closetoc.onclick = function(){
+    sidebar.classList.add('fade-out');
+    closetoc.classList.add('fade-out');
+    toTopMobile.classList.add('fade-out');
+    opentoc.classList.add('fade-in');
+    sidebar.classList.remove('fade-in');
+    closetoc.classList.remove('fade-in');
+    toTopMobile.classList.remove('fade-in');
+    opentoc.classList.remove('fade-out');
 }
